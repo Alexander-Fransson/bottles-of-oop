07aec95776b292442a83e1ea1c537c70af8589e3
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-09-29 15:45:23 +0200

    testar 99-3 och 2

--- bottlesOfBear.js
+++ bottlesOfBear.js
@@ -16,21 +16,21 @@ module.exports = class Bottles{
                     'No more bottles of beer on the wall, '+
                     'no more bottles of beer.\n'+
                     'Go to the store and buy some more, '+
-                    '99 bottles of beer on the wall\n'
+                    '99 bottles of beer on the wall.\n'
                 );
             case 1:
                 return(
                     '1 more bottle of beer on the wall, '+
                     '1 more bottle of beer.\n'+
                     'Take it down and pass it around, '+
-                    'no more bottles of bear on the wall\n'
+                    'no more bottles of bear on the wall.\n'
                 );
             case 2:
                 return(
-                    '2 more bottles of beer on the wall ,'+
-                    '2 more bottles of beer.\n'+
+                    '2 bottles of beer on the wall, '+
+                    '2 bottles of beer.\n'+
                     'Take one down and pass it around, '+
-                    '1 more bottle of beer on the wall\n'
+                    '1 more bottle of beer on the wall.\n'
                 );
                         
             default:
--- bottlesOfBear.test.js
+++ bottlesOfBear.test.js
@@ -9,4 +9,22 @@ describe('Bottles', () => {
         `98 bottles of beer on the wall.\n`;
         expect(new Bottles().verse(99)).toBe(expected);
     });
+
+    test('the third last verse', () => {
+        const expected = 
+        `3 bottles of beer on the wall, `+
+        `3 bottles of beer.\n`+
+        `Take one down and pass it around, `+
+        `2 bottles of beer on the wall.\n`;
+        expect(new Bottles().verse(3)).toBe(expected);
+    });
+
+    test('the second last verse', () => {
+        const expected = 
+        `2 bottles of beer on the wall, `+
+        `2 bottles of beer.\n`+
+        `Take one down and pass it around, `+
+        `1 more bottle of beer on the wall.\n`;
+        expect(new Bottles().verse(2)).toBe(expected);
+    });
 });
