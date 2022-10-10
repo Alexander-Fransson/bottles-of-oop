d48453049307923f20fa19091bf235cd4348a8cd
AlexanderFransson7 <70211809+festusOp7@users.noreply.github.com>
2022-10-10 13:30:02 +0200

    kapittel 3 klart

--- bottlesOfBear.js
+++ bottlesOfBear.js
@@ -1,3 +1,22 @@
+/*
+classer ska vara solida vileket står för
+    * Single pupouse
+    * Open-close (att de ska var öppna för att expandera sina syften men att man inte ska behöva ändra så mycket av den existerande koden)
+    * Liskov substitution (att de ska kunna göra allt som deras föräldrar kann)
+    * Interface segregation (att de inte ska vara beroende av metoder de inte använder)
+    * Dependency inversion (att de ska bero på abstrakta saker)
+*/
+
+
+/*
+namn på funktioner ska vara en abstraktion av funktionens potentiella output 
+tänk att alla outputs som kan komma ska kuna läggas under en kolumn i ett schema 
+*/
+
+/*
+med flocking menas att komplexa beteenden ska komma från änkla regler
+*/
+
 module.exports = class Bottles{
     song(){
         return this.verses(99,0);
@@ -17,36 +36,44 @@ module.exports = class Bottles{
         return numbers;
     }
 
+    container(number){
+        if(number === 1){
+            return 'bottle';
+        }else{
+            return 'bottles';
+        }
+    }
+
     verse(number){
         switch (number) {
             case 0:
                 return(
-                    'No more bottles of beer on the wall, '+
-                    'no more bottles of beer.\n'+
+                    `No more ${this.container(number)} of beer on the wall, `+
+                    `no more ${this.container(number)} of beer.\n`+
                     'Go to the store and buy some more, '+
-                    '99 bottles of beer on the wall.\n'
+                    `${number + 99} ${this.container(number + 99)} of beer on the wall.\n`
                 );
             case 1:
                 return(
-                    '1 more bottle of beer on the wall, '+
-                    '1 more bottle of beer.\n'+
+                    `1 more ${this.container(number)} of beer on the wall, `+
+                    `1 more ${this.container(number)} of beer.\n`+
                     'Take it down and pass it around, '+
-                    'no more bottles of bear on the wall.\n'
+                    `no more ${this.container(number -1)} of bear on the wall.\n`
                 );
             case 2:
                 return(
-                    '2 bottles of beer on the wall, '+
-                    '2 bottles of beer.\n'+
-                    'Take one down and pass it around, '+
-                    '1 more bottle of beer on the wall.\n'
+                    `${number} ${this.container(number)} of beer on the wall, `+
+                    `${number} ${this.container(number)} of beer.\n`+
+                    `Take one down and pass it around, `+
+                    `${number -1} more ${this.container(number -1)} of beer on the wall.\n`
                 );
                         
             default:
                 return(
-                    `${number} bottles of beer on the wall, `+
-                    `${number} bottles of beer.\n`+
+                    `${number} ${this.container(number)} of beer on the wall, `+
+                    `${number} ${this.container(number)} of beer.\n`+
                     `Take one down and pass it around, `+
-                    `${number -1} bottles of beer on the wall.\n`
+                    `${number -1} ${this.container(number -1)} of beer on the wall.\n`
                 );
         }
     }
